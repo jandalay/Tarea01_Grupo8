bafec6c546767470fa93902a7a0823150fdb4104
jandalay <jandalay@espol.edu.ec>
2026-08-08 17:23:09 -0500

    Pruebas unitarias en Notificador

--- src/test/NotificadorTest.java
+++ src/test/NotificadorTest.java
@@ -4,41 +4,60 @@ import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import observer.*;
 import composite.*;
 
 public class NotificadorTest {
+    Notificador notificadorEmail;
+    Notificador notificadorWhatsapp;
+    
     @Test
     @DisplayName("test valido en notificación de WhatsApp")
-    public void whatsappValido() {
-        Notificador notificador = new WhatsAppNotificador();
-        Usuario usuario1 = new Usuario("A1", "Andrés", "andre04@mail.com", "+593123");
-        Usuario usuario2 = new Usuario("A2", "Manuel", "manuel_cuenca@mail.com", "+593135");
-        Usuario usuario3 = new Usuario("A3", "Lopez", "lopez_obrador@mail.com", "+593246");
-        notificador.agregarUsuario(usuario1);
-        notificador.agregarUsuario(usuario2);
-        notificador.agregarUsuario(usuario3);
-
-        //mensaje individual
-        Usuario usuarioEnviado = new Usuario("A2", "Manuel", "manuel_cuenca@mail.com", "+593135");
-        notificador.enviarNotificacion(usuarioEnviado, "Estimado usuario, se ha cambiado...");
-
-        //mensaje a todos
-        notificador.enviarNotificacionMasivo("se notifica a todos que debido a...");
+    @BeforeEach
+    public void agregarDatosParaTests() {
+        
+        if (notificadorEmail == null) {
+            notificadorEmail = new EmailNotificador();
+        }
+        if (notificadorWhatsapp == null) {
+            notificadorWhatsapp = new WhatsAppNotificador();
+        }
+        
+        //notificadorEmail
+        Usuario usuario1 = new Usuario("E1", "Ortega", "jacinto_ortega@mail.com", "+593456");
+        Usuario usuario2 = new Usuario("E2","Suarez", "suarez09@mail.com", "+593468");
+        Usuario usuario3 = new Usuario("E3", "Samuel", "samuel_26A@mail.com", "+593579");
+        notificadorEmail.agregarUsuario(usuario1);
+        notificadorEmail.agregarUsuario(usuario2);
+        notificadorEmail.agregarUsuario(usuario3);
+        
+        //notificadorWhatsapp
+        Usuario usuario4 = new Usuario("A1", "Andrés", "andre04@mail.com", "+593123");
+        Usuario usuario5 = new Usuario("A2", "Manuel", "manuel_cuenca@mail.com", "+593135");
+        Usuario usuario6 = new Usuario("A3", "Lopez", "lopez_obrador@mail.com", "+593246");
+        notificadorWhatsapp.agregarUsuario(usuario1);
+        notificadorWhatsapp.agregarUsuario(usuario2);
+        notificadorWhatsapp.agregarUsuario(usuario3);
 
     }
 
     @Test
     @AfterEach
+    public void EmailError() {
+    
+        Usuario usuarioEnviado = new Usuario("E9", "Moran", "moran_bonilla@mail.com", "+593987");
+       try {
+            notificadorEmail.enviarNotificacion(usuarioEnviado, "Estimado usuario, se le notifica...");
+       }
+       catch(IllegalArgumentException ex) {
+        assertEquals("El usuario " + usuarioEnviado.getNombre() + " no está suscrito a las notificaciones por correo electrónico.", ex.getMessage());
+       }
+    }
+
+    @Test
     public void whatsappError() {
-        Notificador notificador = new WhatsAppNotificador();
-        Usuario usuario1 = new Usuario("A1", "Andrés", "andre04@mail.com", "+593123");
-        Usuario usuario2 = new Usuario("A2","Manuel", "manuel_cuenca@mail.com", "+593135");
-        Usuario usuario3 = new Usuario("A3", "Lopez", "lopez_obrador@mail.com", "+593246");
-        notificador.agregarUsuario(usuario1);
-        notificador.agregarUsuario(usuario2);
-        notificador.agregarUsuario(usuario3);
 
         Usuario usuarioEnviado = new Usuario("B2026", "Manuel", "manuel_cuenca@mail.com", "+593135");
         /*
@@ -47,7 +66,7 @@ public class NotificadorTest {
         assertEquals("El usuario " + usuarioEnviado.getNombre() + " no está suscrito a las notificaciones por WhatsApp.", ex.getMessage());
         */
        try {
-            notificador.enviarNotificacion(usuarioEnviado, "Estimado usuario, se ha cambiado...");
+            notificadorWhatsapp.enviarNotificacion(usuarioEnviado, "Estimado usuario, se ha cambiado...");
        }
        catch(IllegalArgumentException ex) {
         assertEquals("El usuario " + usuarioEnviado.getNombre() + " no está suscrito a las notificaciones por WhatsApp.", ex.getMessage());
@@ -56,41 +75,26 @@ public class NotificadorTest {
     }
 
     @Test
-    @AfterEach
-    public void EmailValido() {
-        Notificador notificador = new EmailNotificador();
-        Usuario usuario1 = new Usuario("E1", "Ortega", "jacinto_ortega@mail.com", "+593456");
-        Usuario usuario2 = new Usuario("E2","Suarez", "suarez09@mail.com", "+593468");
-        Usuario usuario3 = new Usuario("E3", "Samuel", "samuel_26A@mail.com", "+593579");
-        notificador.agregarUsuario(usuario1);
-        notificador.agregarUsuario(usuario2);
-        notificador.agregarUsuario(usuario3);
+    public void WhatsappValido() {
+        notificadorWhatsapp.removerUsuario(new Usuario("A1", "Andrés", "andre04@mail.com", "+593123"));
 
         //mensaje individual
         Usuario usuarioEnviado = new Usuario("E3", "Samuel", "samuel_26A@mail.com", "+593579");
-        notificador.enviarNotificacion(usuarioEnviado, "Se le informa que...");
+        notificadorWhatsapp.enviarNotificacion(usuarioEnviado, "Se le informa que su cuenta...");
 
         //mensaje a todos
-        notificador.enviarNotificacionMasivo("Se informa a todos un...");
+        notificadorWhatsapp.enviarNotificacionMasivo("El siguiente comunicado es porque...");
     }
 
     @Test
-    @AfterEach
-    public void EmailError() {
-        Notificador notificador = new EmailNotificador();
-        Usuario usuario1 = new Usuario("E1", "Ortega", "jacinto_ortega@mail.com", "+593456");
-        Usuario usuario2 = new Usuario("E2","Suarez", "suarez09@mail.com", "+593468");
-        Usuario usuario3 = new Usuario("E3", "Samuel", "samuel_26A@mail.com", "+593579");
-        notificador.agregarUsuario(usuario1);
-        notificador.agregarUsuario(usuario2);
-        notificador.agregarUsuario(usuario3);
+    public void EmailValido() {
 
-        Usuario usuarioEnviado = new Usuario("E9", "Moran", "moran_bonilla@mail.com", "+593987");
-       try {
-            notificador.enviarNotificacion(usuarioEnviado, "Estimado usuario, se le notifica...");
-       }
-       catch(IllegalArgumentException ex) {
-        assertEquals("El usuario " + usuarioEnviado.getNombre() + " no está suscrito a las notificaciones por correo electrónico.", ex.getMessage());
-       }
+        //mensaje individual
+        Usuario usuarioEnviado = new Usuario("A3", "Lopez", "lopez_obrador@mail.com", "+593246");
+        notificadorEmail.enviarNotificacion(usuarioEnviado, "Se le informa que...");
+
+        //mensaje a todos
+        notificadorEmail.enviarNotificacionMasivo("Se informa a todos un...");
     }
+
 }
